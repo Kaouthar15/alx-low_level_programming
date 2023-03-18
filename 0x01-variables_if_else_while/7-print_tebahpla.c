3e9d66227298ccfc248b57795075b71b35c38af5
Kaouthar15 <elazamikaoutar@gmail.com>
2023-03-18 10:58:58 -0700

    zyxwvutsrqponmlkjihgfedcba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main- Entry point
+*
+*Return: always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122 ; i >= 97 ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
