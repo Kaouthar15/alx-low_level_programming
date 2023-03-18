540841a795b70a557c724e33fe5baf13edd8b48c
Kaouthar15 <elazamikaoutar@gmail.com>
2023-03-18 04:47:47 -0700

    rrr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main -Entry point
- *
- *Return: Always 0 (Success)
- */
+*main - main entry point
+*return:always 0 means success
+*/
 int main(void)
 {
-	puts("\"Programming is like building a 
-multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
