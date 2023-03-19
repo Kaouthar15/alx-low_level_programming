7de01c88d4980dccde2d17f08bcc958776f146ca
Kaouthar15 <elazamikaoutar@gmail.com>
2023-03-19 10:55:03 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,25 +5,20 @@
 */
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0 ; j < 100 ; j++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0 ; j < 100 ; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
