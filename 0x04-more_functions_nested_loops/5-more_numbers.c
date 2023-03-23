7a446d32e0e65d339917c0cd10c65d65ca579c35
Kaouthar15 <elazamikaoutar@gmail.com>
2023-03-23 13:05:57 -0700

    task(

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*more_numbers- print from 1 to 14 ten times
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
