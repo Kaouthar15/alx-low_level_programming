b6cc1a2b2e8bd0c41ace1800e5c988b2102b09b9
Kaouthar15 <elazamikaoutar@gmail.com>
2023-04-05 13:45:42 -0700

    rr

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,25 +3,29 @@
 *is_prime_number-if the number is prime or not
 *@n:paramétre
 *Return:0 or 1
+*is_prime_helper-if the num 
+*@n:para
+*@d:para
+*Return:1 or 0 or solution
 */
+int is_prime_helper(int n, int d)
+{
+	if (d == n)
+	{
+		return (1);
+	}
+	if (n % d == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, d + 1));
+}
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n < 2)
 	{
 		return (0);
 	}
-	if (n == 2)
-	{
-		return (1);
-	}
-	for (i = 3; i * i <= n; i += 2)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_prime_helper(n, 2));
 }
