051049b87e2f9d2f45b5f00f9de13f29893e1c08
Kaouthar15 <elazamikaoutar@gmail.com>
2023-04-17 10:31:29 -0700

    tt

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+  *struct dog - a dog's basic info
+  *@name :First member
+  *@age: Second member
+  *@owner: Third Member
+  *
+  *Description : Longer 
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
